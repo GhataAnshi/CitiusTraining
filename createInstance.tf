9ec99f8144ee9c403b8d2a972cb9c4a8f33d8ac4
GhataS <ghatasaxena27@gmail.com>
2021-10-14 20:20:42 +0530

    Centralized creds

--- createInstance.tf
+++ createInstance.tf
@@ -1,8 +1,3 @@
-provider "aws"{
-    access_key = "AKIAQTI2CRNTCCYBPT5W"
-    secret_key = "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"
-    region = "ap-south-1"
-}
 
 resource "aws_instance"  "MyFirstInstance" {
     ami = "ami-041d6256ed0f2061c"
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws"{
+    access_key = "AKIAQTI2CRNTCCYBPT5W"
+    secret_key = "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"
+    region = "ap-south-1"
+}
